d30eea9a788fb143a6099807a32ccee4b86b1667
Faizan Kamal <faizankhan619.fk@gmail.com>
2025-03-21 08:48:37 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# Staging Server Configuration
+STAGING_CONFIG = {
+    "ssh_host": "ssh.test.ausbildungsbasis.de",
+    "ssh_username": "u2260-suoehce9to92",
+    "ssh_password": "Ausbildungsbasis123?.",
+    "ssh_port": 18765,
+    "db_host": "test.ausbildungsbasis.de",  # Database host for Staging
+    "db_user": "uaixkdmalwgpa",
+    "db_password": "Ausbildungsbasis123?.",
+    "db_name": "dbbjv8sgihuufp"
+}
+
+# Live Server Configuration
+LIVE_CONFIG = {
+    "ssh_host": "ssh.app.ausbildungsbasis.de",
+    "ssh_username": "u2515-gmvd9q5gkmdh",
+    "ssh_password": "Ausbildungsbasis123?.",
+    "ssh_port": 18765,
+    "db_host": "app.ausbildungsbasis.de",  # Database host for Live
+    "db_user": "uyyebhqdm5kd0",
+    "db_password": "Ausbildungsbasis123?.",
+    "db_name": "dbatq0nvzx9a14"
+}
